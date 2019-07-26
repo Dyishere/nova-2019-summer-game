a18ea643c3d785818e3efe5f9351c306b137b26c
FINCTIVE <FINCTIVE@qq.com>
2019-07-27 00:07:12 +0800

    踩头

--- Assets/Nova/Scripts/Character/BirdPlatformerMovement.cs
+++ Assets/Nova/Scripts/Character/BirdPlatformerMovement.cs
@@ -16,27 +16,32 @@ public class BirdPlatformerMovement : MonoBehaviour
     public float jumpForce;
     public float flyingSpeed; // 羽落状态下增加的速度
     public float flyingBaseSpeed; // 羽落状态下与输入无关的初始速度
-    public float flyingUpForce;
+    public float flyingUpForce; // 空气对鸟翼的支持力（浮力？）
 
     [Space(10)]
 
-    [Header("高级属性")]
     //与游戏内容无关的变量
-    private const int maxJumpTimes = 2; // 可以连跳的次数
+
+    [Header("高级属性")]
     [SerializeField] private float jumpBreakTime = .3f; // 两次跳跃之间的间隔时间
-    [SerializeField] private GameObject CharacterSprite; // 将角色精灵分离设置成子物体，这样在转身的时候能保持父级物体的Scale始终为正值
+    [SerializeField] private GameObject CharacterSprite; // 将角色精灵分离设置成子物体，这样在转身的时候能保持父级物体的Scale始终为正值(预防潜在bug?)
     [SerializeField] private Transform GroundCheck;
     [SerializeField] private float groundCheckRadius = .3f;
     [SerializeField] private LayerMask m_WhatIsGround;
+    [SerializeField] private float hurtVelocityY = 0.2f; // 被踩头的时候，对方的掉落速度最小值
+    [SerializeField] [Range(0f, 1f)] private float headGetHurtRange = 0.9f; // 属于可以被踩的头的范围是多少（头部边缘踩到后不属于有效攻击）
+    [SerializeField] private float recoveringTime = 1f; // 被攻击后需要的恢复时间，期间为眩晕状态，无法操作
 
     [Space(10)]
 
     // [状态变量]
+    private const int maxJumpTimes = 2; // 可以连跳的次数
+    private bool isFacingRight = true;
     private bool isGrounded = false;
     private int jumpCounter = 0; // 记录跳跃的次数（用于连跳）
     private float jumpTimer = 0f; // 记录跳跃后经过的时间
     private bool isFlying = false;
-    private bool isFacingRight = true;
+    private bool isHurt = false; // 被攻击状态/眩晕状态, 无法操作, 短时间内不会被继续伤害
 
     private Rigidbody2D m_Rigidbody2D;
     private Animator m_Animator;
@@ -55,6 +60,13 @@ public class BirdPlatformerMovement : MonoBehaviour
 
     public void Move(float iMove, bool iJump)
     {
+        // 眩晕状态，无法操作
+        if (isHurt)
+        {
+            return;
+        }
+
+
         // 计算以下两个变量，然后应用变化
         Vector2 moveDir = new Vector2(iMove * moveSpeed, 0);
         bool jump = false;
@@ -131,5 +143,50 @@ public class BirdPlatformerMovement : MonoBehaviour
         // Vector2 position2D = new Vector2(transform.position.x, transform.position.y);
         // m_Rigidbody2D.MovePosition(m_Rigidbody2D.position + moveDir * Time.deltaTime);
         m_Animator.SetFloat("move", Mathf.Abs(iMove));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // 潜在BUG，Contcts数组可能不止一个元素，有可能有碰撞没有被捕捉到
+        // 被踩头
+        PlayerController m_player = other.gameObject.GetComponent<PlayerController>();
+        if (m_player && other.rigidbody.velocity.y < -1f * hurtVelocityY)
+        {
+            CapsuleCollider2D capsule = GetComponent<CapsuleCollider2D>();
+            float headRadius = capsule.size.x;
+            float playerHeight = capsule.size.y;
+
+            // 踩到圆形头部的上半部分 算作有效攻击
+            float dx = other.GetContact(0).point.x - transform.position.x;
+
+            // 接触点到头中心的距离差（transform.position在脚底）
+            float dy = other.GetContact(0).point.y - (transform.position.y + playerHeight - headRadius);
+
+            if (dy > 0f && (Mathf.Abs(dx) < headRadius * headGetHurtRange))
+            {
+                // 这部分可能会连续触发
+                GetHurt();
+            }
+            Debug.Log(other.contacts.Length);
+        }
+    }
+
+    private void GetHurt()
+    {
+        if (isHurt)
+            return;
+
+        isHurt = true;
+
+        // 眩晕部分代码
+        m_Animator.SetBool("hurt", true);
+
+        Invoke("Recover", recoveringTime);
+    }
+
+    private void Recover()
+    {
+        isHurt = false;
+        m_Animator.SetBool("hurt", false);
     }
 }
