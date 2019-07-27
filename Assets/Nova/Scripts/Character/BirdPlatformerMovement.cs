95f7b6b238ed6c57bba6f75f7bb6b8daad97b7ff
FINCTIVE <FINCTIVE@qq.com>
2019-07-27 22:03:56 +0800

    冲刺

--- Assets/Nova/Scripts/Character/BirdPlatformerMovement.cs
+++ Assets/Nova/Scripts/Character/BirdPlatformerMovement.cs
@@ -26,26 +26,27 @@ public class BirdPlatformerMovement : MonoBehaviour
 
     [Header("高级属性")]
     [SerializeField] private float jumpBreakTime = .3f; // 两次跳跃之间的间隔时间
-    [SerializeField] private GameObject CharacterSprite; // 将角色精灵分离设置成子物体，这样在转身的时候能保持父级物体的Scale始终为正值(预防潜在bug?)
-    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private GameObject CharacterSprite = null; // 将角色精灵分离设置成子物体，这样在转身的时候能保持父级物体的Scale始终为正值(预防潜在bug?)
+    [SerializeField] private Transform GroundCheck = null;
     [SerializeField] private float groundCheckRadius = .3f;
-    [SerializeField] private LayerMask m_WhatIsGround;
+    [SerializeField] private LayerMask m_WhatIsGround = 0;
     [SerializeField] private float hurtVelocityY = 0.2f; // 被踩头的时候，对方的掉落速度最小值
     [SerializeField] [Range(0f, 1f)] private float headGetHurtRange = 0.9f; // 属于可以被踩的头的范围是多少（头部边缘踩到后不属于有效攻击）
     [SerializeField] private float recoveringTime = 1f; // 被攻击后需要的恢复时间，期间为眩晕状态，无法操作
 
     [Space(10)]
-
     // [状态变量]
+    [Header("角色状态")]
+    public bool isDashing = false;
     private const int maxJumpTimes = 2; // 可以连跳的次数
     private bool isFacingRight = true;
     private bool isGrounded = false;
     private int jumpCounter = 0; // 记录跳跃的次数（用于连跳）
-    private float jumpTimer = 0f; // 记录跳跃后经过的时间
+    private float jumpTimer = 0f; // 记录跳跃瞬间后经过的时间
     private bool isFlying = false;
     private bool dashed = false; // 在这次滑翔中是否已经冲刺过
-    public bool isDashing = false;
-    private bool isHurt = false; // 被攻击状态/眩晕状态, 无法操作, 短时间内不会被继续伤害
+    private float dashTimer = 0f;
+    private bool isHurt = false; // 被攻击后的眩晕状态, 无法操作, 短时间内不会被继续受伤害
     private string touchingProp = "null";        //玩家碰触的物品名字
     private bool isPicking = false;              //是否正在拿着东西
 
@@ -76,16 +77,15 @@ public class BirdPlatformerMovement : MonoBehaviour
     {
         bool iDash = Input.GetKeyDown(KeyCode.J); // 测试用的代码,之后会替换掉
 
-        // 眩晕状态，无法操作
-        if (isHurt)
+        // 眩晕状态或者冲刺阶段，禁用控制
+        if (isHurt || isDashing)
         {
-            return;
+            iMove = 0f;
+            iJump = false;
         }
-
-
         // 计算以下两个变量，然后应用变化
-        Vector2 moveDir = new Vector2(iMove * moveSpeed, 0);
-        bool jump = false;
+        Vector2 moveDir = new Vector2(iMove * moveSpeed, 0); // 移动方向
+        bool jump = false; // 是否跳跃
 
         // 跳跃
         isGrounded = false;
@@ -96,7 +96,7 @@ public class BirdPlatformerMovement : MonoBehaviour
             {
                 isGrounded = true;
                 dashed = false;
-                if (m_Rigidbody2D.velocity.y < 0f)
+                if (m_Rigidbody2D.velocity.y < 0f) //防止起跳瞬间执行
                 {
                     jumpCounter = 0;
                 }
@@ -128,7 +128,7 @@ public class BirdPlatformerMovement : MonoBehaviour
             jumpTimer = 0f;
         }
 
-        // 羽落效果
+        // 羽落效果(滑翔)
         if (isGrounded || jumpCounter == 2)
         {
             isFlying = false;
@@ -143,18 +143,25 @@ public class BirdPlatformerMovement : MonoBehaviour
         if (isFlying)
         {
             moveDir += new Vector2(iMove * flyingSpeed + facingDir * flyingBaseSpeed, 0);
+
             m_Rigidbody2D.AddForce(flyingUpForce * Vector2.up);
 
-            if (!dashed && iDash)
+            if (iDash && !dashed && !isDashing)
             {
-                // 冲刺状态变化 如“金身模式”，全身发光？？？（未实现）
-                // 加力出现历史遗留BUG！！移动方式为直接修改velocity所以加力之后只会瞬移！
-                // m_Rigidbody2D.AddForce(facingDir * Vector2.right * dashForce);
+                m_Rigidbody2D.AddForce(facingDir * Vector2.right * dashForce);
                 m_Animator.SetBool("dash", true);
                 isDashing = true;
-                Invoke("FinishDashing", dashTime); // 如果直接落地了呢？
                 dashed = true;
+                iDash = false;
+                dashTimer = 0f;
             }
+        }
+
+        dashTimer += Time.deltaTime;
+        if (dashTimer > dashTime || !isFlying)
+        {
+            isDashing = false;
+            m_Animator.SetBool("dash", false);
         }
 
         // 翻转
@@ -166,17 +173,16 @@ public class BirdPlatformerMovement : MonoBehaviour
             transform.Find("PickPos").transform.localPosition = new Vector2(transform.Find("PickPos").transform.localPosition.x * -1, 1);
         }
 
-        // 移动
-        m_Rigidbody2D.velocity = moveDir * 100f * Time.deltaTime + m_Rigidbody2D.velocity.y * Vector2.up;
-        // 如果需要水平后座力可以考虑使用注释中的代码（有bug）
-        // Vector2 position2D = new Vector2(transform.position.x, transform.position.y);
-        // m_Rigidbody2D.MovePosition(m_Rigidbody2D.position + moveDir * Time.deltaTime);
+        // 移动(冲刺状态下不能直接修改velocity)
+        if (!isDashing)
+        {
+            m_Rigidbody2D.velocity = moveDir * 100f * Time.deltaTime + m_Rigidbody2D.velocity.y * Vector2.up;
+        }
         m_Animator.SetFloat("move", Mathf.Abs(iMove));
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        // 潜在BUG，Contcts数组可能不止一个元素，有可能有碰撞没有被捕捉到
         // 被踩头
         PlayerController m_player = other.gameObject.GetComponent<PlayerController>();
         if (m_player && other.rigidbody.velocity.y < -1f * hurtVelocityY)
@@ -184,32 +190,37 @@ public class BirdPlatformerMovement : MonoBehaviour
             CapsuleCollider2D capsule = GetComponent<CapsuleCollider2D>();
             float headRadius = capsule.size.x;
             float playerHeight = capsule.size.y;
-
-            // 踩到圆形头部的上半部分 算作有效攻击
-            float dx = other.GetContact(0).point.x - transform.position.x;
-
-            // 接触点到头中心的距离差（transform.position在脚底）
-            float dy = other.GetContact(0).point.y - (transform.position.y + playerHeight - headRadius);
-
-            if (dy > 0f && (Mathf.Abs(dx) < headRadius * headGetHurtRange))
+            bool isHit = false;
+            foreach (ContactPoint2D m_contact in other.contacts)
             {
-                // 这部分可能会连续触发
+                // 踩到圆形头部的上半部分 算作有效攻击
+                Vector2 contactPos = m_contact.point;
+                // 接触点到头中心的距离差（transform.position在脚底）
+                float dx = contactPos.x - transform.position.x;
+                float dy = contactPos.y - (transform.position.y + playerHeight - headRadius);
+
+                if (dy > 0f && (Mathf.Abs(dx) < headRadius * headGetHurtRange))
+                {
+                    isHit = true;
+                    break;
+                }
+            }
+            if (isHit)
+            {
                 GetHurt();
             }
-            Debug.Log("Contcts数组元素个数：" + other.contacts.Length.ToString());
         }
     }
 
     private void GetHurt()
     {
-        if (isHurt)
+        if (isHurt) // 防止多次受伤
             return;
 
         isHurt = true;
 
         // 眩晕部分代码
         m_Animator.SetBool("hurt", true);
-
         Invoke("Recover", recoveringTime);
     }
 
@@ -219,11 +230,6 @@ public class BirdPlatformerMovement : MonoBehaviour
         m_Animator.SetBool("hurt", false);
     }
 
-    private void FinishDashing()
-    {
-        isDashing = false;
-        m_Animator.SetBool("dash", false);
-    }
     public void PickUp(bool iPick, int curPlayerNum)        //PlayerController调用，传入当前的玩家编号
     {
 
