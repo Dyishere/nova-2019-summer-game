56af5b62ae3bb644eee68ee9034939db585dab66
sushuo <1640991897@qq.com>
2019-08-10 20:29:45 +0800

    Tese_Scenes_2场景添加，需要大佬修改人物移动和发射键

---
+++ Assets/Nova/Scripts/Scenes_2/Line.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer LR;
+    public GameObject bullet;
+    public GameObject BulletMaster;
+    public bool IsShotting = false;
+    public bool startRound = false;
+
+    Bullet bullet_;
+    private void Start()
+    {
+        bullet_ = BulletMaster.GetComponent<Bullet>();
+        LR.SetColors(Color.black,Color.black);
+        LR.SetWidth(0.1f, 0.1f);       
+    }
+    
+    
+    private void Update()
+    {
+        LR.SetPosition(0, gameObject.transform.localPosition);
+        LR.SetPosition(1, bullet.transform.localPosition);
+
+        if (Input.GetKeyDown(KeyCode.J) && startRound == true)         //如果按下J并且已经开始旋转，那么发射子弹
+        {
+            IsShotting = true;
+            bullet_.enAbleToCatchPlayer = true;
+        }
+                                                                       //如果按下K并且没有旋转，没有发射，那么旋转
+        if (Input.GetKeyDown(KeyCode.K) && IsShotting == false && startRound == false)
+        {
+            if(bullet_.Stop == true)
+            {
+                bullet_.Stop = false;
+            }
+
+            bullet.transform.position = new Vector3(transform.position.x + 2, transform.position.y, 0);
+
+            startRound = true;
+            IsShotting = false;
+            bullet_.enAbleToCatchPlayer = false;
+        }
+    }
+}
